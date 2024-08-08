4099d171b420c14307780cca8f4330daf55cb88d
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-08 13:04:11 +0530

    Add pseudocode for Caesar Cipher

---
+++ caesar-cipher.rb
@@ -0,0 +1,14 @@
+def caesar_cipher(string, shift_factor)
+  # PSEUDOCODE
+  # SET a variable to an empty string
+  # FOR every char in the string
+  #   IF char is an alphabet THEN
+  #     OBTAIN integer value of char
+  #     ADD shift_factor to integer value
+  #     OBTAIN new char based on integer value
+  #     CONCATENATE new char to the new variable
+  #   ELSE
+  #     CONCATENATE new char to the new variable
+  #   ENDIF
+  # ENDFOR
+end
